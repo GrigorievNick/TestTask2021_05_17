d3f6242877cb6d1e0f40d49b390923c86a3bdb2f
mykola.hryhoriev <grigor60@gmail.com>
2021-05-17 13:43:33 +0300

    Add Agg test case

--- src/main/scala/org/test/task/titlingpoint/Main.scala
+++ src/main/scala/org/test/task/titlingpoint/Main.scala
@@ -1,59 +1,88 @@
 package org.test.task.titlingpoint
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.streaming.StreamingQuery
-import org.apache.spark.sql.types.{DataType, StructType}
+import org.apache.spark.sql.streaming.{DataStreamWriter, OutputMode, StreamingQuery}
+import org.apache.spark.sql.types.{DataType, DecimalType, StructType}
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 // TODO split to source and sink
-case class ETLConf(inputSparkSchemaInJson: String,
+case class ETLConf(outputTablePath: String,
+                   checkpointLocationPath: String,
                    timestampColumn: String, // TODO timestmap pattern configuration.
-                   orderColumn: Option[String],
                    datePartitionColumn: String,
                    hourPartitionColumn: String,
                    mappingExpression: List[String])
 
+case class RevenuSumConf(outputTablePath: String, checkpointLocationPath: String)
+
 case class AppConfig(inputDataPath: String,
-                     outputTablePath: String,
-                     checkpointLocationPath: String,
-                     recordsInFile: Long,
-                     etlConf: ETLConf)
+                     inputSparkSchemaInJson: String,
+                     etl: ETLConf,
+                     revenueSum: RevenuSumConf)
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     // TODO log input configs
-    // TODO Ficus or Pure config mapping from HOCON
     implicit val sparkSession: SparkSession = SparkSession.builder().config(new SparkConf()).getOrCreate()
-    val stream = runETLStream(null)
-    stream.awaitTermination() // TODO graceful shutdown
+    // TODO Ficus or Pure config mapping from HOCON to case class
+    runETLsStream(null)
+    runRevenueAggStream(null)
+
+    // TODO graceful shutdown
+    while(sparkSession.streams.active.length > 0) sparkSession.streams.awaitAnyTermination()
+
   }
 
-  def runETLStream(conf: AppConfig)(implicit spark: SparkSession): StreamingQuery = {
-    val ETLConf(inputSchema, timestampColumn, _, datePartitionColumn, hourPartitionColumn, mappingExpression) =
-      conf.etlConf
+  def runRevenueAggStream(conf: AppConfig)(implicit spark: SparkSession): StreamingQuery = {
+    val RevenuSumConf(outputTablePath, checkpointLocationPath) = conf.revenueSum
+    spark
+      .readStream
+      .schema(DataType.fromJson(conf.inputSparkSchemaInJson).asInstanceOf[StructType])
+      .json(conf.inputDataPath)
+      // TODO move to config
+      // TODO migrate to Dataset API
+      .groupBy("user").agg(sum(col("spend").cast(DecimalType(32,2))).as("total_spend"))
+      .writeStream
+      .option("checkpointLocation", checkpointLocationPath)
+      .format("delta") // To simplify schema evolution
+      .outputMode(OutputMode.Complete()) // add calculation per day and compaction process
+      .start(outputTablePath)
+  }
 
+  def runETLsStream(conf: AppConfig)(implicit spark: SparkSession): StreamingQuery = {
+    val ETLConf(outputTablePath, checkpointLocationPath,
+    timestampColumn, datePartitionColumn, hourPartitionColumn, mappingExpression) = conf.etl
     // Because test task does not have any information about file structure of data on S3, I make thing very simple to me.
     // Pay attention that previously, this deliver exactly once only if EMRFS used or table format like _spark_metadata or delta.
     // But few month ago AWS announce that s3 is not eventually consistent any more. So this solution may work.
     spark
       .readStream
-      .schema(DataType.fromJson(inputSchema).asInstanceOf[StructType]) // TODO in real world scenario must be swiped to normal schema managment
+      .schema(DataType.fromJson(conf.inputSparkSchemaInJson).asInstanceOf[StructType]) // TODO in real world scenario must be swiped to normal schema managment
       // TODO configure backpressure options to avoid OOM exception or disk io overhelming
       .json(conf.inputDataPath)
       .select((timestampColumn :: mappingExpression).map(col): _*) // Apply etls
       .withColumn(datePartitionColumn, date_format(col(timestampColumn), "yyyy-MM-dd"))
       .withColumn(hourPartitionColumn, hour(col(timestampColumn)))
-      // TODO improve filesize and ordering in spark 3 for non streaming application
-      //      .repartitionByRange(datePartitionColumn, hourPartitionColumn, orderColumn, )
       .writeStream
-      .option("checkpointLocation", conf.checkpointLocationPath)
-
+      .option("checkpointLocation", checkpointLocationPath)
+      .outputMode(OutputMode.Append)
       .format("delta") // To simplify schema evolution
       .partitionBy(datePartitionColumn, hourPartitionColumn)
-
-      .start(conf.outputTablePath)
+      .start(outputTablePath)
 
   }
+
+  implicit class SparkDataWriterScalaSamWorkAround[T](writer: DataStreamWriter[T]) {
+
+    /**
+     * Support Scala Native function2 for foreachBatch, root cause
+     * https://docs.databricks.com/release-notes/runtime/7.0.html#!#other-behavior-changes
+     */
+    def foreachBatchNS(function: (Dataset[T], Long) => Unit): DataStreamWriter[T] =
+      writer.foreachBatch(function)
+
+  }
+
 }
--- src/test/scala/RunLocally.scala
+++ src/test/scala/RunLocally.scala
@@ -3,14 +3,17 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.json.JsonFileFormat
 import org.apache.spark.sql.execution.streaming.FileStreamSink
-import org.test.task.titlingpoint.{AppConfig, ETLConf, Main}
+import org.test.task.titlingpoint.{AppConfig, ETLConf, Main, RevenuSumConf}
 
 object RunLocally {
 
   def main(args: Array[String]): Unit = {
     val baseTestPath = "/tmp/titlingPoint/"
     val mockedJsonDataPath = s"$baseTestPath/input_data"
-    val deltaTablePath = s"$baseTestPath/delta_table"
+    val userBasePath = s"$baseTestPath/user"
+    val userTablePath = s"$userBasePath/table"
+    val revenueBasePath = s"$baseTestPath/revenue"
+    val revenueTablePath = s"$revenueBasePath/table"
 
     // prepare test env.
     implicit val spark: SparkSession = SparkSession.builder().master("local[*]").getOrCreate()
@@ -18,25 +21,32 @@ object RunLocally {
 
     // prepare data
     val mockedData = spark.read.option("inferSchema", "true").json("src/test/resources/input-data.json")
-    new FileStreamSink(spark, mockedJsonDataPath, new JsonFileFormat(), Seq.empty, Map.empty)
-      .addBatch(0, mockedData)
-
+    val mockDataStream = new FileStreamSink(spark, mockedJsonDataPath, new JsonFileFormat(), Seq.empty, Map.empty)
+    mockDataStream.addBatch(0, mockedData)
 
     val config = AppConfig(
       inputDataPath = mockedJsonDataPath,
-      outputTablePath = deltaTablePath,
-      checkpointLocationPath = s"$baseTestPath/checkpoint",
-      recordsInFile = 2000000,
-      etlConf = ETLConf(inputSparkSchemaInJson = mockedData.schema.prettyJson,
+      inputSparkSchemaInJson = mockedData.schema.prettyJson,
+      etl = ETLConf(outputTablePath = userTablePath,
+        checkpointLocationPath = s"$userBasePath/checkpoint",
         timestampColumn = "timestamp",
-        orderColumn = None, datePartitionColumn = "date",
+        datePartitionColumn = "date",
         hourPartitionColumn = "hour",
-        mappingExpression = List("user", "evtname")))
-    val stream = Main.runETLStream(config)
+        mappingExpression = List("user", "evtname")),
+      revenueSum = RevenuSumConf(
+        outputTablePath = revenueTablePath,
+        checkpointLocationPath = s"$revenueBasePath/checkpoint")
+    )
+    val user = Main.runETLsStream(config)
+    user.processAllAvailable()
+    spark.read.format("delta").load(userTablePath).show()
 
-    stream.processAllAvailable()
-
-    spark.read.format("delta").load(deltaTablePath).show()
+    val revenue = Main.runRevenueAggStream(config)
+    revenue.processAllAvailable()
+    spark.read.format("delta").load(revenueTablePath).show()
+    mockDataStream.addBatch(1, mockedData)
+    revenue.processAllAvailable()
+    spark.read.format("delta").load(revenueTablePath).show()
   }
 
 }
